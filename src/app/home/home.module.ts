ef7cb0224a8f4c7158ffba5a861de823df7727b5
Gugun Gunawan <gugun.net21@gmail.com>
2023-04-27 21:18:51 +0700

    --

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,3 +1,22 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { IonicModule } from '@ionic/angular';
+// import { FormsModule } from '@angular/forms';
+// import { HomePage } from './home.page';
+
+// import { HomePageRoutingModule } from './home-routing.module';
+
+
+// @NgModule({
+//   imports: [
+//     CommonModule,
+//     FormsModule,
+//     IonicModule,
+//     HomePageRoutingModule
+//   ],
+//   declarations: [HomePage]
+// })
+// export class HomePageModule {}
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
@@ -5,9 +24,11 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     CommonModule,
     FormsModule,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+declare var google;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  map: any;
 
-  constructor() {}
+  constructor(private platform: Platform) {}
 
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      this.initMap();
+    });
+  }
+
+  initMap() {
+    const mapOptions = {
+      zoom: 12 // set zoom level
+    };
+
+    this.map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+    // get current position of device
+    navigator.geolocation.getCurrentPosition((position) => {
+      const location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      // center map on user's location
+      this.map.setCenter(location);
+
+      // add marker to user's location
+      const marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Your location'
+      });
+    }, (error) => {
+      console.log('Error getting location', error);
+    });
+  }
 }
